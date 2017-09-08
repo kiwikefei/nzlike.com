864ffac0798886be93e5260bdf825e7db4c1403c
Andy Tang <kiwikefei@gmail.com>
2017-09-09 11:18:37 +1200

    tweak slackMessage

--- public/_deploy.php
+++ public/_deploy.php
@@ -53,8 +53,7 @@ class Deployer
 //        echo ($output);
         $link = "https://github.com/kiwikefei/nzlike.com/commit/{$_SERVER['HTTP_X_GITHUB_DELIVERY']}";
         $commitMessage = $this->notifyTo($this->payload->head_commit->committer->name) . "\n"
-            . "New delivery <{$this->server}>\n"
-            . " ---------------------------------------------\n";
+            . " New delivery for {$this->server} \n";
 
         foreach($this->payload->commits as $commit) {
             $committer = $commit->committer->name != $this->payload->head_commit->committer->name ?
