4cc32ce7eb509e6a6730a510ffbd65169b93b89c
Andy Tang <kiwikefei@gmail.com>
2017-09-08 20:38:58 +1200

    tweak layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,9 +14,8 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
+                color: #000;
+                font-family: 'Lato', sans-serif;
                 height: 100vh;
                 margin: 0;
             }
