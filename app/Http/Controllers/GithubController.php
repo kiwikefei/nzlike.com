24340ab181e29f949b53210539ea7c96864e4004
Andy Tang <kiwikefei@gmail.com>
2017-09-11 07:56:20 +1200

    payload debug

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -8,7 +8,10 @@ class GithubController extends Controller
 {
     public function callback(Request $request)
     {
-        $payload = $request->payload;
-        echo(gettype($payload));
+        $xEvent = $request->header('X-GitHub-Event');
+        $payload = json_decode($request->getContent());
+        echo ("{$xEvent}\n");
+        echo ("{$payload->ref}\n");
+        echo ("{$payload}\n");
     }
 }
