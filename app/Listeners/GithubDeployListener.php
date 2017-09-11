a79108f0236650b4e6024212d2c769e83eb6d573
Andy Tang <kiwikefei@gmail.com>
2017-09-11 23:01:50 +1200

    should queue

--- app/Listeners/GithubDeployListener.php
+++ app/Listeners/GithubDeployListener.php
@@ -6,7 +6,7 @@ use App\Events\GithubPushed;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class GithubDeployListener
+class GithubDeployListener implements ShouldQueue
 {
     /**
      * Create the event listener.
