51d89b501a6eaca33b9ee5f953e03bd8ddac1b10
Andy Tang <kiwikefei@gmail.com>
2017-09-12 11:13:08 +1200

    deployment refactor

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\GithubPushed' => [
-            'App\Listeners\GithubDeployListener',
+        'App\Events\Something' => [
+            'App\Listeners\SomethingListener',
         ],
     ];
 
