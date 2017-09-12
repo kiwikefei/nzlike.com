c86cbf4a9111c4514cbce0b811d86fc762656952
Andy Tang <kiwikefei@gmail.com>
2017-09-12 12:41:56 +1200

    add wording

--- app/Services/Deployment/DeployWebsiteJob.php
+++ app/Services/Deployment/DeployWebsiteJob.php
@@ -47,11 +47,14 @@ class DeployWebsiteJob implements ShouldQueue
         }
 
         if($this->needRunNpm()){
+            \Log:info('npm run required');
             foreach($this->npmCommands as $command) {
-                \Log::info('npm command => '. $command . 'starting...');
+                \Log::info('command => '. $command . 'starting...');
                 shell_exec($command);
-                \Log::info('npm command => '. $command . ' done.');
+                \Log::info('command => '. $command . ' done.');
             }
+        }else{
+            \Log:info('no npm run required');
         }
         \Log::info('All commands executed.');
     }
