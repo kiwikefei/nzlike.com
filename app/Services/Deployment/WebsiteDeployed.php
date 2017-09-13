5621f3d323cf2bf2f382f96a98d43efff9091b91
Andy Tang <kiwikefei@gmail.com>
2017-09-14 00:16:40 +1200

    time format

--- app/Services/Deployment/WebsiteDeployed.php
+++ app/Services/Deployment/WebsiteDeployed.php
@@ -59,7 +59,7 @@ class WebsiteDeployed extends Notification
             ->attachment(function($attachment) use( $deployment) {
                 $attachment->title("Click to see changes", $deployment['changes'])
                     ->fields($deployment['fields'])
-                    ->footer('github')
+                    ->footer('Pushed from github')
                     ->timestamp($deployment['timestamp']);
             });
 
